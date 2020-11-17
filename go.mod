b2896f03b68fda43e4c8504106af92fc4eb2c9ad
Nat Welch <nat@natwelch.com>
2020-11-17 04:28:48 +0000

    main

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/icco/tab-archive
+
+go 1.15
+
+require (
+	contrib.go.opencensus.io/exporter/stackdriver v0.13.4
+	github.com/felixge/httpsnoop v1.0.1
+	github.com/go-chi/chi v1.5.0
+	github.com/go-chi/cors v1.1.1
+	github.com/hellofresh/logging-go v0.4.0
+	github.com/icco/logrus-stackdriver-formatter v0.3.0
+	github.com/lib/pq v1.8.0
+	github.com/sirupsen/logrus v1.7.0
+	go.opencensus.io v0.22.5
+)
---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"database/sql"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"contrib.go.opencensus.io/exporter/stackdriver"
+	"contrib.go.opencensus.io/exporter/stackdriver/monitoredresource"
+	"contrib.go.opencensus.io/exporter/stackdriver/propagation"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"github.com/icco/tab-archive/lib"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	log = lib.InitLogging()
+)
+
+func main() {
+	port := "8080"
+	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+		port = fromEnv
+	}
+	log.Infof("Starting up on http://localhost:%s", port)
+
+	if os.Getenv("ENABLE_STACKDRIVER") != "" {
+		labels := &stackdriver.Labels{}
+		labels.Set("app", "tab-archive", "The name of the current app.")
+		sd, err := stackdriver.NewExporter(stackdriver.Options{
+			ProjectID:               "icco-cloud",
+			MonitoredResource:       monitoredresource.Autodetect(),
+			DefaultMonitoringLabels: labels,
+			DefaultTraceAttributes:  map[string]interface{}{"app": "relay"},
+		})
+
+		if err != nil {
+			log.WithError(err).Fatalf("failed to create the stackdriver exporter")
+		}
+		defer sd.Flush()
+
+		view.RegisterExporter(sd)
+		trace.RegisterExporter(sd)
+		trace.ApplyConfig(trace.Config{
+			DefaultSampler: trace.AlwaysSample(),
+		})
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("cannot connect to database server: %+v", err)
+	}
+	defer db.Close()
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+	r.Use(lib.LoggingMiddleware())
+
+	crs := cors.New(cors.Options{
+		AllowCredentials:   true,
+		OptionsPassthrough: false,
+		AllowedOrigins:     []string{"*"},
+		AllowedMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders:     []string{"Accept", "Authorization", "Content-Type"},
+		ExposedHeaders:     []string{"Link"},
+		MaxAge:             300, // Maximum value not ignored by any of major browsers
+	})
+	r.Use(crs.Handler)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi."))
+	})
+
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi."))
+	})
+
+	r.Post("/hook", func(w http.ResponseWriter, r *http.Request) {
+		ct := r.Header.Get("content-type")
+		log.WithField("content-type", ct).Debug("got content-type")
+
+		buf, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.WithError(err).Error("could not read buffer")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if ct != "application/json" {
+			http.Error(w, "expected 'application/json' content type", http.StatusBadRequest)
+			return
+		}
+
+		if err := lib.ParseAndStore(r.Context, db, buf); err != nil {
+			log.WithError(err).Error("could not parse request")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write([]byte(`{"status": "success"}`))
+	})
+
+	h := &ochttp.Handler{
+		Handler:     r,
+		Propagation: &propagation.HTTPFormat{},
+	}
+	if err := view.Register([]*view.View{
+		ochttp.ServerRequestCountView,
+		ochttp.ServerResponseCountByStatusCode,
+	}...); err != nil {
+		log.WithError(err).Fatal("Failed to register ochttp views")
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, h))
+}
