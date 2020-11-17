aa44811e6ce91ba734760df0dbf3bbbce3c34590
Nat Welch <nat@natwelch.com>
2020-11-17 04:31:38 +0000

    init

---
+++ lib/parser.go
@@ -0,0 +1,11 @@
+package lib
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+func ParseAndStore(ctx context.Context, db *sql.Database, buf []byte) error {
+	return fmt.Errorf("not implemented")
+}
