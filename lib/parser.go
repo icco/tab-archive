3559417c918a5d31ffc5fd44dd34f2de985d38bb
Nat Welch <nat@natwelch.com>
2020-11-18 02:35:28 +0000

    parse

--- lib/parser.go
+++ lib/parser.go
@@ -3,9 +3,25 @@ package lib
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"time"
 )
 
+type Tab struct {
+	URL     string
+	Title   string
+	Favicon string
+	Seen    time.Time
+}
+
 func ParseAndStore(ctx context.Context, db *sql.DB, buf []byte) error {
-	return fmt.Errorf("not implemented")
+	log.WithField("body", string(buf)).Debug("attempting to parse")
+	var t *Tab
+	if err := json.Unmarshal(buf, &t); err != nil {
+		return fmt.Errorf("could not parse: %w", err)
+	}
+
+	log.WithField("tab", t).Info("parsed")
+	return nil
 }
